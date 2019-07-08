8a1a99440c2c5814d60ca19065fe09dde0cb7ef2
Matias Zeitune <matias.zeitune@despegar.com>
2019-07-08 12:34:45 -0300

    controller to receive new image

--- src/bulmapsaur/main/controllers.py
+++ src/bulmapsaur/main/controllers.py
@@ -1,25 +1,34 @@
-from flask import Flask
-from flask import request, jsonify
-import json
+import base64
+import cv2
+import jsonpickle
+import numpy as np
+from flask import Flask, Response
+from flask import request
 
 app = Flask(__name__)
 
-
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
-
-  def myfunc(self):
-    print("Hello my name is " + self.name)
+def stringToImage(base64_string):
+    decoded_string = base64.b64decode(base64_string)
+    np_arr = np.fromstring(decoded_string, np.uint8)
+    return cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
 
+@app.route('/notify-new-image', methods=['POST'])
+def notifyNewImage():
+    #example encode base 64
+    #filename = "/home/matiaszeitune/Descargas/hoja.jpg"
+    #with open(filename, "rb") as fid:
+    #    data = fid.read()
 
-@app.route('/postjson', methods=['GET'])
-def postJsonHandler():
-    p1 = Person("John", 36)
-    p1.myfunc()
-    return json.dumps(p1.__dict__)
+    #b64_bytes = base64.b64encode(data)
+    #print(b64_bytes)
+    img = stringToImage(request.data)
+
+    response = {'message': 'image received. size={}x{}'.format(img.shape[0], img.shape[1])}
+
+    response_pickled = jsonpickle.encode(response)
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
 
 
 app.run(port=8090)
