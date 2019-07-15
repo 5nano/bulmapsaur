b396cc875b0fc1bcab060911e67a35b15256f29e
lucasAmoroso <lucas.amoroso@despegar.com>
2019-07-14 22:29:10 -0300

    Added logger

--- src/bulmapsaur/main/image_controller.py
+++ src/bulmapsaur/main/image_controller.py
@@ -1,13 +1,18 @@
 import asyncio
 import json
+import logging
+from logging.config import fileConfig
+from threading import Thread
 
 import jsonpickle
 from flask import Flask, Response, request
 from flask_cors import CORS
 from flask_sslify import SSLify
-from threading import Thread
 
 import image_service as imageService
+
+fileConfig('config/logging_config.ini',disable_existing_loggers=False)
+logger = logging.getLogger()
 
 app = Flask(__name__)
 CORS(app)
@@ -18,19 +23,22 @@ def start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
     asyncio.set_event_loop(loop)
     loop.run_forever()
 
-
-@app.route('/bulmapsaur/api/images', methods=['POST'])
-def processImage():
+def asyncImageProcessing(imageName,imageB64):
     loop = asyncio.new_event_loop()
     t = Thread(target=start_background_loop, args=(loop,), daemon=True)
-    t.start()    
+    t.start()  
+    asyncio.run_coroutine_threadsafe(imageService.processImage(imageName,imageB64), loop)
+
+@app.route('/bulmapsaur/api/images', methods=['POST'])
+def processImage(): 
     data = request.data
     imageInfo = json.loads(data)
     imageName = imageInfo.get('name')
     imageB64 = imageInfo.get('base64')
+    logger.info("Image %s received",imageName)
     #En algun momento va a correr
-    asyncio.run_coroutine_threadsafe(imageService.processImage(imageName,imageB64), loop)
+    asyncImageProcessing(imageName,imageB64)
     return Response(response=jsonpickle.encode({'message': 'image received'}), status=200, mimetype="application/json")
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0',port = '8443',ssl_context = context,debug = True)
+    app.run(host = '0.0.0.0',port = '8443',ssl_context = context)
--- src/bulmapsaur/main/image_service.py
+++ src/bulmapsaur/main/image_service.py
@@ -1,10 +1,17 @@
-import encode_utils as  encodeUtils
+import logging
+from logging.config import fileConfig
+
 import image_analyzer as imageAnalyzer
-import file_utils as fileUtils
+import utils.encode_utils as encodeUtils
+import utils.file_utils as fileUtils
+
+fileConfig('config/logging_config.ini',disable_existing_loggers=False)
+logger = logging.getLogger()
 
 
 async def processImage(imageName,imageB64):
-    print("Processing image ...")
+    logger.info("Processing image %s ...",imageName)
     img_decoded = encodeUtils.base64Decode(imageB64)
     fileUtils.saveImage(imageName,img_decoded)
     #imageAnalyzer.analyze(imageName+".jpg")
+    logger.info("Image %s succesfully processed ",imageName)
