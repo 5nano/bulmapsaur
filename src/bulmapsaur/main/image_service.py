c43f1515e7e85b4afdfbcfc1d7ea7ab2c95eb8e7
Lucas Amoroso <lucas.amoroso@despegar.com>
2019-08-31 19:43:54 -0300

    Update image_service.py

--- src/bulmapsaur/main/image_service.py
+++ src/bulmapsaur/main/image_service.py
@@ -11,7 +11,7 @@ from cassandra_connector import insert
 
 
 async def processImage(idAssay,idExperiment,imageB64):
-    app_log.info("Processing image with idTest %s and idPlant %s...",idAssay,idExperiment)
+    app_log.info("Processing image with idAssay %s and idExperiment %s...",idAssay,idExperiment)
     img_decoded = base64Decode(imageB64)
     now = datetime.now()
     dt_string = now.strftime("%d-%m-%Y-%H-%M-%S")
